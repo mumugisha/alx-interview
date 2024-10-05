95327114ac81e197164975d7f4bb9487221ed5d0
mumugisha <prudent.nkubito@gmail.com>
2024-10-05 14:54:44 +0300

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for a in range(1, n):
         row = [1]
         for b in range(1, a):
             row.append(triangle[a-1][b-1] + triangle[a-1][b])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
