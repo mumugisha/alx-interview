70fd9e44d659da173ffcc9bc9bb271d17e051e63
mumugisha <prudent.nkubito@gmail.com>
2024-10-12 10:43:34 +0300

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ The Pascal Triangle Project"""
 
+
 def pascal_triangle(n):
     """ Return a list of lists representing Pascal's Triangle"""
     if n <= 0:
@@ -13,11 +14,11 @@ def pascal_triangle(n):
         current_row = [0] * (a + 1)
         current_row[0] = 1
         current_row[-1] = 1
-        
+
         # Fill the internal elements of the row if there are any
         for b in range(1, a):
             current_row[b] = p_triangle[a - 1][b - 1] + p_triangle[a - 1][b]
-                
+
         p_triangle[a] = current_row
-        
+
     return p_triangle
