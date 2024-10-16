3f2006da9edcdc327a81a2c1383b4aed4e1f9f0e
mumugisha <prudent.nkubito@gmail.com>
2024-10-16 17:32:47 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,61 +1,61 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 '''Calculate the minimum number of operations
-required to reach exactly n 'H' characters in a file,
+required to reach exactly n 'H' characters in a file, 
 using only two operations: copy-all and paste.
 '''
 
 
 def minOperations(n):
-    '''Calculates the fewest number of
-    operations needed to result in exactly n H
-    characters in this file.
-    
+    '''Compute the minimum number of operations 
+    needed to reach exactly `n` 'H' characters using 
+    only copy-all and paste operations.
     Returns:
-        Integer: if n is impossible to achieve, return 0
+        Integer : if n is impossible to achieve, return 0
     '''
-    pasted_chars = 1  # Number of 'H' characters currently in the file.
-    clipboard = 0  # Clipboard stores the copied number of 'H' characters.
-    counter = 0  # Counter to track the total number of operations.
+    past_chars = 1  # Number of 'H' characters currently in the file
+    new_clipboard = 0  # Clipboard stores the copied number of 'H' characters
+    first_counter = 0  # Counter to track the total number of operations.
 
-    while pasted_chars < n:
-        # If the clipboard is empty, did not copy any operation.
-        if clipboard == 0:
+    while past_chars < n:
+        # If the clipboard is empty
+        if new_clipboard == 0:
             # Copy-all 'H' to clipboard
-            clipboard = pasted_chars
+            new_clipboard = past_chars
             # Increment the counter for the copy operation
-            counter += 1
+            first_counter += 1
 
         # If only one 'H' character is in the file.
-        if pasted_chars == 1:
+        if past_chars == 1:
             # Paste from the clipboard
-            pasted_chars += clipboard
+            past_chars += new_clipboard
             # Increment the counter for the paste operation
-            counter += 1
+            first_counter += 1
             # Continue to the next iteration of the loop
             continue
 
-        remaining = n - pasted_chars  # Calculate remaining chars to be pasted.
-        # If the remaining characters are less than the clipboard content,
+        remaining = n - past_chars  # Calculate remaining chars to be pasted.
+        # If the remaining characters are less 
+        # than the clipboard content,
         # return 0 as it's not possible to reach n.
-        if remaining < clipboard:
+        if remaining < new_clipboard:
             return 0
 
-        # If remaining characters are not divisible by pasted_chars
-        if remaining % pasted_chars != 0:
-            # Paste current clipboard content
-            pasted_chars += clipboard
-            # Increment the counter for the paste operation
-            counter += 1
+        # If can't be divided
+        if remaining % past_chars != 0:
+            # Paste current clipboard
+            past_chars += new_clipboard
+            # Increment operations counter
+            first_counter += 1
         else:
-            # Copy all the current number of 'H' characters
-            clipboard = pasted_chars
-            # Paste the clipboard content
-            pasted_chars += clipboard
-            # Increment for both copy and paste operations
-            counter += 2
+            # Copy all
+            new_clipboard = past_chars
+            # Paste
+            past_chars += new_clipboard
+            # Increment operations counter
+            first_counter += 2
 
-    # If the target number of characters is reached
-    if pasted_chars == n:
-        return counter
+    # If got the desired result
+    if past_chars == n:
+        return first_counter
     else:
         return 0
