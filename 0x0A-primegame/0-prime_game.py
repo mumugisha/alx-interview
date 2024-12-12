1e0539224e283a081098f3ff47176b2db9dec51f
mumugisha <prudent.nkubito@gmail.com>
2024-12-12 15:50:37 +0200

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -66,7 +66,7 @@ def isPrime(n):
     """
     if n < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n**(1/2)+1,2):
         if n % i == 0:
             return "Not prime"
     return True
