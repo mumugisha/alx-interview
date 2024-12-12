044cd668da047d257ed97f34a7ff3fad84c22543
mumugisha <prudent.nkubito@gmail.com>
2024-12-12 13:27:10 +0200

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,37 +4,33 @@
 
 def isWinner(x, nums):
     """Determines the winner of the prime game.
-
     Args:
         x (int): Number of rounds.
         nums (list): List of integers for each round.
-
     Returns:
         str: Name of the winner ('Maria' or 'Ben'), or None if no winner.
     """
     if not nums or x < 1:
         return None
 
-    counting_winner = {'Maria': 0, 'Ben': 0}
+    counting_winner = {"Maria": 0, "Ben": 0}
 
     for round_num in nums[:x]:
         round_winner = isRoundWinner(round_num)
         if round_winner:
             counting_winner[round_winner] += 1
 
-    if counting_winner['Maria'] > counting_winner['Ben']:
-        return 'Maria'
-    elif counting_winner['Ben'] > counting_winner['Maria']:
-        return 'Ben'
+    if counting_winner["Maria"] > counting_winner["Ben"]:
+        return "Maria"
+    elif counting_winner["Ben"] > counting_winner["Maria"]:
+        return "Ben"
     return None
 
 
 def isRoundWinner(n):
     """Determines the winner for a single round.
-
     Args:
         n (int): The upper bound of the range for the round.
-
     Returns:
         str: Name of the round winner ('Maria' or 'Ben'), or None.
     """
@@ -42,7 +38,7 @@ def isRoundWinner(n):
         return None
 
     numbers = list(range(1, n + 1))
-    players = ['Maria', 'Ben']
+    players = ["Maria", "Ben"]
     turn = 0  # Maria starts
 
     while numbers:
@@ -63,16 +59,14 @@ def isRoundWinner(n):
 
 def isPrime(num):
     """Checks if a number is prime.
-
     Args:
         num (int): The number to check.
-
     Returns:
         bool: True if the number is prime, False otherwise.
     """
     if num < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
